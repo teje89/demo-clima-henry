8347963ce4ef4d29167ca2e66904d27fb982abdb
Hernan Tejedor <hernant@live.com>
2022-05-04 15:22:09 -0300

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './components/Card.jsx';
+import Cards from './components/Cards.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import data, { Cairns } from './data.js';
+import Nav from './components/Nav.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <SearchBar
+          onSearch={(ciudad) => alert(ciudad)}
+        />
+      </div>
+      {/*<div>
+        <Card
+          max={Cairns.main.temp_max}
+          min={Cairns.main.temp_min}
+          name={Cairns.name}
+          img={Cairns.weather[0].icon}
+          onClose={() => alert(Cairns.name)}
+        />
+  </div>*/} 
+      <hr />
+      <div>
+        <Cards
+          cities={data}
+        />
+      </div>
+      <hr />
+
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Logo from '../logoHenry.png'
+import SearchBar from './SearchBar.jsx';
+//import './Nav.css';
+
+function Nav({onSearch}) {
+  return (
+    <div>
+        <SearchBar onSearch={onSearch}/>
+    </div>
+  );
+};
+
+export default Nav;
